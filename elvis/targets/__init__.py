7548aa3821d102292f7796838361787623955f1f
Kieran Leschinski <kdleschinski@gmail.com>
2025-06-03 13:35:41 +0200

    added tests for SED Template classes

--- elvis/targets/__init__.py
+++ elvis/targets/__init__.py
@@ -1,1 +1,2 @@
 from .morphology import *
+from .sed import *
--- elvis/tests/test_server.py
+++ elvis/tests/test_server.py
@@ -4,17 +4,17 @@ import io
 from pathlib import Path
 from astropy.io import fits
 
-
 from elvis.server import app  # import the Flask app
 
-
 JSON_FILENAME = Path(__file__).parent / "data/eris_nix.json"
+
 
 @pytest.fixture
 def client():
     app.config["TESTING"] = True  # Optional: Flask runs in testing mode
     with app.test_client() as client:
         yield client
+
 
 def test_fits_content_from_response(client):
     with open(JSON_FILENAME, "r") as file:
@@ -24,6 +24,7 @@ def test_fits_content_from_response(client):
 
     assert response.status_code == 200
     assert response.mimetype == "application/fits"
+
 
 def test_fits_header_has_keyword_from_input_json(client):
     with open(JSON_FILENAME, "r") as file:
